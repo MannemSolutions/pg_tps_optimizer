54dee3d78076fbf6da9b5d99d1e1f6a8336b796e
Sebastiaan Mannem <sebas@mannem.nl>
2023-05-18 19:59:16 +0200

    Fixing rust release workflow

--- Makefile
+++ Makefile
@@ -1,4 +1,5 @@
 build:
+	./set_version.sh
 	cargo build
 
 run:
--- set_version.sh
+++ set_version.sh
@@ -3,4 +3,5 @@ VERSION=$(echo $GITHUB_REF | cut -d / -f 3)
 if [ -z "${VERSION}" ]; then
   VERSION=$(git tag | sort -V | grep '^v' | tail -n1)
 fi
+VERSION=$(echo $VERSION | sed 's/[^0-9.]//g')
 sed -i "s/^version = .*/version = \"${VERSION}\"/" Cargo.toml

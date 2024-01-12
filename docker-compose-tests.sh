c9c5e499b62e7c1c5ca5f50a290f93d61829afdb
Sebastiaan Mannem <sebas@mannem.nl>
2024-01-12 10:13:35 +0100

    fixing docker-compose tests, as they clearly return false possitives

--- docker-compose-tests.sh
+++ docker-compose-tests.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
-set -e
+set -ex
 
 docker-compose down --remove-orphans || echo new or partial install
 docker rmi pg_tps_optimizer-pg_tps_optimizer || echo image was not there
 if [ ! -e config/tls/int_server/certs/localhost.pem ]; then
   rm -rf config/tls
+  chmod 777 config
   docker-compose up chainsmith
 fi
 docker-compose up -d postgres
@@ -15,10 +16,4 @@ for ((i=0;i<60;i++)); do
 done
 
 cargo test -- --include-ignored
-
-export PGUSER=postgres
-export PGHOST=127.0.0.1
-export PGSSLCERT=config/tls/int_client/certs/postgres.pem
-export PGSSLKEY=config/tls/int_client/private/postgres.key.pem
-export PGSSLROOTCERT=config/tls/int_server/certs/ca-chain-bundle.cert.pem
 docker-compose up pg_tps_optimizer
